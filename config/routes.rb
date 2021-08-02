bddaf4e668928f4d9d512cc74b7bbc8885d638bd
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-08-03 09:13:45 +1000

    Created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :users
+  post '/login' => 'auth#login'
+  get '/auth' => 'auth#persist'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
